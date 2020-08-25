bd61dbb6d5322112f5de40f3e00fa1b8939f4a11
Ondřej Čertík <ondrej@certik.us>
2020-08-25 17:22:50 -0600

    Disable march=native
    Fixes #11.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ set -ex
 
 CXXFLAGS="${CXXFLAGS} -D__STDC_FORMAT_MACROS" cmake \
     -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS_RELEASE="-Wall -Wextra -O3 -funroll-loops -DNDEBUG" \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DWITH_LLVM=yes \
