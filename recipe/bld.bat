48f1d0a0716cd702b4cdfb33d7bce2d487a5f954
Ondřej Čertík <ondrej@certik.us>
2020-10-08 17:51:10 -0600

    Set XJUPYTER_DATA_DIR

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,12 @@
-cmake .. -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D WITH_LLVM=yes -DWITH_XEUS=yes %SRC_DIR%
+cmake ^
+    -G "NMake Makefiles" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DXJUPYTER_DATA_DIR=%PREFIX%\\share\\jupyter ^
+    -DCMAKE_PREFIX_PATH=%PREFIX% ^
+    -DWITH_LLVM=yes ^
+    -DWITH_XEUS=yes ^
+    %SRC_DIR%
 if errorlevel 1 exit 1
 
 cmake --build . --target install
