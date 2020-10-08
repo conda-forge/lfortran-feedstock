25ef95f1ece87732eaf23dea8b168e1f724745d5
Ondřej Čertík <ondrej@certik.us>
2020-10-08 17:50:39 -0600

    Use cmake to build & install the project

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,5 @@
 cmake .. -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D WITH_LLVM=yes -DWITH_XEUS=yes %SRC_DIR%
 if errorlevel 1 exit 1
 
-nmake
-if errorlevel 1 exit 1
-
-nmake install
+cmake --build . --target install
 if errorlevel 1 exit 1
